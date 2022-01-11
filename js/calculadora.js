525e1e16022c802cc26591a38d58fd06773cfdfb
ArthurCost4 <arthurlinhares69@gmail.com>
2022-01-11 15:33:46 -0300

    Umas alterações no CSS3 e HTML5
    +Organizado/+Formatado

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,36 +1,3 @@
-/* let n1 = ""
-let n2 = ""
-let metodo = ""
-let resulta = ""
-
-function calcular(ev) {
-
-    n1 = parseFloat(document.getElementById("num1").value);
-    n2 = parseFloat(document.getElementById("num2").value);
-    metodo = document.getElementById("forma").value;
-    resulta = document.getElementById("resulta");
-
-    if(metodo == "+"){
-        resulta = n1 + n2
-        window.alert(`${resulta}`)
-    }
-    else if (metodo == "-"){
-        resulta = n1 - n2
-        window.alert(`${resulta}`)
-    }
-    else if (metodo == "*"){
-        resulta = n1 * n2
-        window.alert(`${resulta}`)
-    }
-    else if (metodo == "/"){
-        resulta = n1 / n2
-        window.alert(`${resulta}`)
-    }
-}
-__________________
-A Antiga interação
-*/
-
 
 //A nova interação
 function displaynum(n1){
